cf5fb3ec1b6d68fc12f29e9c46e7c47a7b2ea124
Pawelec97 <pawel.wis1997@wp.pl>
2020-05-02 21:31:23 +0200

    japierdole

---
+++ src/main/java/pl/Pawelec97/WebApplication2PW/BookRepository.java
@@ -0,0 +1,9 @@
+package pl.Pawelec97.WebApplication2PW;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository  extends JpaRepository {
+
+}
